c244aefc824294739bc33a48a01e12bff804c39c
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-10 08:37:25 -0400

    fix(efi) : fixed a weird choice of input parameters in efibootmgr

--- programs/dev/boot/efibootmgr.nix
+++ programs/dev/boot/efibootmgr.nix
@@ -1,13 +1,14 @@
 {
   config,
   lib,
+  pkgs-list,
   oldPathNames,
   ...
 } @ Inputs: let
   name = "efibootmgr";
   main-repo = "nix";
   branch = "latest";
-  packages = Inputs.pkgs-list.${main-repo}.${branch};
+  packages = pkgs-list.${main-repo}.${branch};
   pathNames = oldPathNames ++ [name];
   fullPath = lib.concatStringsSep "." pathNames;
   cfg = config.${name};
