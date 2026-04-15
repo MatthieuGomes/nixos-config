e031e1d26eda3b89fe463bdffda6e3943e99710d
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-15 18:24:59 -0400

    feat(build) : give more ressources for vm testing

--- new_arch/configuration.nix
+++ new_arch/configuration.nix
@@ -40,6 +40,13 @@ in {
   virtualisation = {
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
+    vmVariant = {
+      # the following configuration is added only when building VM with `build-vm`
+      virtualisation = {
+        memorySize = 2048; # use 2048MiB memory
+        cores = 3; # use 3 cpu cores
+      };
+    };
   }; # FIXME : Move to somewhere else
   fonts.packages = with pkgs-list.nix.latest; [nerd-fonts.jetbrains-mono]; # FIXME : Move to somewhere else
 }
