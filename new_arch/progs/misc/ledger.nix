be77fd03e8b89e33fdaf043acc0dd2533cd7c5db
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-15 20:01:13 -0400

    feat(ledger) : added ledger to misc

--- new_arch/progs/misc/ledger.nix
+++ new_arch/progs/misc/ledger.nix
@@ -7,33 +7,17 @@
   ...
 }: let
   ######  # user defined
-  name = "misc";
-  subfolder = "misc";
+  name = "ledger";
+  subfolder = null;
   main-repo = "nix";
   branch = "latest";
-  imports = [
-    "ledger"
-    # "bambu-studio"
-    # "bitwarden"
-    # "fastfetch"
-    # "iso-image-writer"
-    # "vesktop"
-    # "kdeconnect"
-  ];
+  imports = null;
   options = {
     enable = lib.mkEnableOption "Enables ${name} program and related settings.";
   };
   extras = {
   };
-  settings = {
-    ledger.enable = true;
-    bambu-studio.enable = true;
-    bitwarden.enable = true;
-    fastfetch.enable = true;
-    iso-image-writer.enable = true;
-    kdeconnect.enable = true;
-    vesktop.enable = true;
-  };
+  settings = null;
   ######  # computed
   packages =
     if main-repo != null && branch != null
@@ -59,10 +43,11 @@
   ######  # user defined
   Home = {
     home.packages = with packages; [
-      vlc
+      ledger-live-desktop
     ];
   };
   System = {
+    hardware.ledger.enable = true;
   };
   ######  # computed
   HomeConfig = Common // Home;
