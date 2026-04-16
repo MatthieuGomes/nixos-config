d87ccae8842b92fe5db7735e5ce6ac98ae4c0966
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-16 09:32:25 -0400

    feat(postman) : added postman to dev

--- new_arch/progs/dev/postman.nix
+++ new_arch/progs/dev/postman.nix
@@ -7,42 +7,17 @@
   ...
 }: let
   ######  # user defined
-  name = "dev";
-  subfolder = "dev";
+  name = "postman";
+  subfolder = null;
   main-repo = "nix";
   branch = "latest";
-  imports = [
-    ## "editors" => # "vim" "vscode"
-    ## "lang" => # "nix"
-    ## "terminals" => # "ghostty"
-    ## "virtualization" => # "docker"
-    ## "network" => # "postman"
-    "nix"
-    "boot"
-    "network"
-    "ghostty"
-    "git" #solo
-    # "vim"
-    # "vscode"
-    "docker"
-    "postman"
-  ];
+  imports = null;
   options = {
     enable = lib.mkEnableOption "Enables ${name} program and related settings.";
   };
   extras = {
   };
-  settings = {
-    nix.enable = true;
-    boot.enable = true;
-    network.enable = true;
-    ghostty.enable = true;
-    git.enable = true;
-    vim.enable = true;
-    vscode.enable = true;
-    docker.enable = true;
-    postman.enable = true;
-  };
+  settings = null;
   ######  # computed
   packages =
     if main-repo != null && branch != null
@@ -68,9 +43,7 @@
   ######  # user defined
   Home = {
     home.packages = with packages; [
-      python3
-      nmap # for network
-      netcat-openbsd # for network
+      postman
     ];
   };
   System = {
