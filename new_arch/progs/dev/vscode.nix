069b684402776571ce303b5b3b53c999fd1540a8
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-16 09:44:16 -0400

    feat(vscode) : added vscode to dev

--- new_arch/progs/dev/vscode.nix
+++ new_arch/progs/dev/vscode.nix
@@ -7,42 +7,17 @@
   ...
 }: let
   ######  # user defined
-  name = "dev";
-  subfolder = "dev";
+  name = "vscode";
+  subfolder = null;
   main-repo = "nix";
-  branch = "latest";
-  imports = [
-    ## "editors" => # "vim" "vscode"
-    ## "lang" => # "nix"
-    ## "terminals" => # "ghostty"
-    ## "virtualization" => # "docker"
-    ## "network" => # "postman"
-    "nix"
-    "boot"
-    "network"
-    "ghostty"
-    "git" #solo
-    "vim"
-    "vscode"
-    "docker"
-    "postman"
-  ];
+  branch = "unstable";
+  imports = null;
   options = {
     enable = lib.mkEnableOption "Enables ${name} program and related settings.";
   };
   extras = {
   };
-  settings = {
-    nix.enable = true;
-    boot.enable = true;
-    network.enable = true;
-    ghostty.enable = true;
-    git.enable = true;
-    vim.enable = true;
-    vscode.enable = true;
-    docker.enable = true;
-    postman.enable = true;
-  };
+  settings = null;
   ######  # computed
   packages =
     if main-repo != null && branch != null
@@ -67,11 +42,14 @@
     };
   ######  # user defined
   Home = {
-    home.packages = with packages; [
-      python3
-      nmap # for network
-      netcat-openbsd # for network
-    ];
+    programs.vscode = {
+      enable = true;
+      package = packages.vscode;
+      mutableExtensionsDir = true;
+      # profiles.Default={
+
+      # };
+    };
   };
   System = {
   };
