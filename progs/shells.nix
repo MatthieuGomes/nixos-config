2d441bfcf0c36adb264f919d0bac0ca90c994831
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-20 13:45:10 -0400

    refactor(shells) : used the new generator to get the new module with no variables

--- progs/shells.nix
+++ progs/shells.nix
@@ -5,78 +5,34 @@
   parentPathAsList,
   tools,
   ...
-}: let
-  ######  # user defined
-  name = "shells";
-  subfolder = "shells";
-  main-repo = "nix";
-  branch = "latest";
-  imports = [
-    "zsh"
-  ];
-  options = {
-    enable = lib.mkEnableOption "Enables ${name} program and related settings.";
-  };
-  extras = {
-    aliases = {
-      nrs = "sudo nixos-rebuild switch";
-      ls = "ls --color -ah";
-      ".." = "cd ..";
-      yazi = "y";
-      myip = "myip";
-    };
-  };
-  settings = {
-    zsh = {
-      enable = true;
-      aliases = extras.aliases;
-    };
-  };
-  ######  # computed
-  packages =
-    if main-repo != null && branch != null
-    then pkgs-list.${main-repo}.${branch}
-    else null;
-  currentPathAsList = parentPathAsList ++ [name];
-  currentDirPath = lib.path.subpath.join (lib.lists.flatten ["./." parentPathAsList]);
-  cfg = tools.inheritConfig {
-    inherit config currentPathAsList;
-  };
-  inheritedSettings =
-    if imports != null || settings != null
-    then
-      tools.inheritSettings {
-        inherit currentPathAsList imports settings;
-      }
-    else {
-    };
-  Common =
-    inheritedSettings
-    // {
-    };
-  ######  # user defined
-  Home = {
-  };
-  System = {
-  };
-  ######  # computed
-  HomeConfig = Common // Home;
-  SystemConfig = Common // System;
-in {
-  config = {
-    Home = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "Home";
-      Config = HomeConfig;
-    };
-    System = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "System";
-      Config = SystemConfig;
-    };
-  };
-}
+}: (tools.fullModule ((tools.moduleParams
+    rec {
+      inherit config lib pkgs-list parentPathAsList tools;
+      name = "shells";
+      subfolder = "shells";
+      main-repo = "nix";
+      branch = "latest";
+      imports = [
+        "zsh"
+      ];
+      options = {
+        enable = lib.mkEnableOption "Enables ${name} program and related settings.";
+      };
+      settings = {
+        zsh = {
+          enable = true;
+          aliases = extras.aliases;
+        };
+      };
+      extras = {
+        aliases = {
+          nrs = "sudo nixos-rebuild switch";
+          ls = "ls --color -ah";
+          ".." = "cd ..";
+          yazi = "y";
+          myip = "myip";
+        };
+      };
+    })
+  // {
+  }))
