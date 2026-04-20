450f3f6f501941bbd24d64f4bf71311dfd64273b
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-20 16:24:38 -0400

    refactor(shells-modules) : refactored all modules with new generators

--- progs/shells/zsh.nix
+++ progs/shells/zsh.nix
@@ -6,60 +6,41 @@
   tools,
   ...
 }: let
-  ######  # user defined
-  name = "zsh";
-  subfolder = "zsh";
-  main-repo = "nix";
-  branch = "latest";
-  imports = [
-    "oh-my-zsh"
-    "yazi"
-    "fzf"
-  ];
-  options = {
-    enable = lib.mkEnableOption "Enables ${name} program and related settings.";
-    aliases = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      description = "An attribute set of shell aliases to define.";
-      default = {};
+  moduleParams = tools.moduleParams rec {
+    inherit config lib pkgs-list parentPathAsList tools;
+    name = "zsh";
+    subfolder = "zsh";
+    main-repo = "nix";
+    branch = "latest";
+    imports = [
+      "oh-my-zsh"
+      "yazi"
+      "fzf"
+    ];
+    options = {
+      enable = lib.mkEnableOption "Enables ${name} program and related settings.";
+      aliases = lib.mkOption {
+        type = lib.types.attrsOf lib.types.str;
+        description = "An attribute set of shell aliases to define.";
+        default = {};
+      };
+    };
+    settings = {
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "sudo"
+        ];
+      };
+      fzf.enable = true;
+      yazi.enable = true;
     };
   };
-  extras = {
-  };
-  settings = {
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "sudo"
-      ];
-    };
-    fzf.enable = true;
-    yazi.enable = true;
-  };
-  ######  # computed
-  packages =
-    if main-repo != null && branch != null
-    then pkgs-list.${main-repo}.${branch}
-    else null;
-  currentPathAsList = parentPathAsList ++ [name];
-  currentDirPath = lib.path.subpath.join (lib.lists.flatten ["./." parentPathAsList]);
-  cfg = tools.inheritConfig {
-    inherit config currentPathAsList;
-  };
-  inheritedSettings =
-    if imports != null || settings != null
-    then
-      tools.inheritSettings {
-        inherit currentPathAsList imports settings;
-      }
-    else {
-    };
-  Common =
-    inheritedSettings
-    // {
-    };
-  ######  # user defined
+in (tools.fullModule rec {
+  inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
+  inherit (moduleParams) name subfolder main-repo branch extras imports specialImports options settings;
+  inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
   Home = {
     home.file.".p10k.zsh".text = "${builtins.readFile "${./${subfolder}/.p10k.zsh}"}";
     programs.zsh = {
@@ -92,24 +73,4 @@
   System = {
     programs.zsh.enable = true;
   };
-  ######  # computed
-  HomeConfig = Common // Home;
-  SystemConfig = Common // System;
-in {
-  config = {
-    Home = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "Home";
-      Config = HomeConfig;
-    };
-    System = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "System";
-      Config = SystemConfig;
-    };
-  };
-}
+})
