b4d93e4cfa38b32b5db0826a8cee48e9c9e116e2
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-20 16:40:15 -0400

    refactor(fs-modules) : refactored all modules with new generators

--- sys/filesystems/btrfs.nix
+++ sys/filesystems/btrfs.nix
@@ -6,41 +6,19 @@
   tools,
   ...
 }: let
-  ######  # user defined
-  name = "btrfs";
-  subfolder = null;
-  main-repo = "nix";
-  branch = "latest";
-  imports = null;
-  options = {
-    enable = lib.mkEnableOption "Enables and configures ${name} filesystem support.";
-  };
-  settings = null;
-  ######  # computed
-  packages =
-    if main-repo != null && branch != null
-    then pkgs-list.${main-repo}.${branch}
-    else null;
-  currentPathAsList = parentPathAsList ++ [name];
-  currentDirPath = lib.path.subpath.join (lib.lists.flatten ["./." parentPathAsList]);
-  cfg = tools.inheritConfig {
-    inherit config currentPathAsList;
-  };
-  inheritedSettings =
-    if imports != null || settings != null
-    then
-      tools.inheritSettings {
-        inherit currentPathAsList imports settings;
-      }
-    else {
+  moduleParams = tools.moduleParams rec {
+    inherit config lib pkgs-list parentPathAsList tools;
+    name = "btrfs";
+    main-repo = "nix";
+    branch = "latest";
+    options = {
+      enable = lib.mkEnableOption "Enables and configures ${name} filesystem support.";
     };
-  Common =
-    inheritedSettings
-    // {
-    };
-  ######  # user defined
-  Home = {
   };
+in (tools.fullModule rec {
+  inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
+  inherit (moduleParams) name subfolder main-repo branch extras imports specialImports options settings;
+  inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
   System = {
     environment.systemPackages = with packages; [
       btrfs-progs
@@ -49,24 +27,4 @@
       btrfs = true;
     };
   };
-  ######  # computed
-  HomeConfig = Common // Home;
-  SystemConfig = Common // System;
-in {
-  config = {
-    Home = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "Home";
-      Config = HomeConfig;
-    };
-    System = tools.contextualModule {
-      inherit lib config tools; # deps
-      inherit subfolder currentPathAsList pkgs-list cfg currentDirPath; # generated
-      inherit imports options; # user defined
-      context = "System";
-      Config = SystemConfig;
-    };
-  };
-}
+})
