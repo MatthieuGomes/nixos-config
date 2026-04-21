e01544bdae2850b85193c2445d900b7cd05487a1
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-20 23:11:32 -0400

    feat(vlc) : adding vlc to misc

--- progs/misc/vlc.nix
+++ progs/misc/vlc.nix
@@ -8,34 +8,20 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "misc";
-    subfolder = "misc";
-    imports = [
-      "ledger"
-      "bambu-studio"
-      "bitwarden"
-      "fastfetch"
-      "iso-image-writer"
-      "vesktop"
-      "kdeconnect"
-      "vlc"
-    ];
+    name = "vlc";
+    main-repo = "nix";
+    branch = "latest";
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
     };
-    settings = {
-      ledger.enable = true;
-      bambu-studio.enable = true;
-      bitwarden.enable = true;
-      fastfetch.enable = true;
-      iso-image-writer.enable = true;
-      kdeconnect.enable = true;
-      vesktop.enable = true;
-      vlc.enable = true;
-    };
   };
-in (tools.fullModule {
+in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  Home = {
+    home.packages = with packages; [
+      vlc # Misc
+    ];
+  };
 })
