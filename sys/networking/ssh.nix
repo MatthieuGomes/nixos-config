b6723f7dc8363ea0448709eab218f3e09aaee447
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-21 10:21:55 -0400

    feat(ssh+vpn) : added ssh+vpn to networking

---
+++ sys/networking/ssh.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  pkgs-list,
+  parentPathAsList,
+  tools,
+  ...
+}: let
+  moduleParams = tools.moduleParams rec {
+    inherit config lib pkgs-list parentPathAsList tools;
+    name = "ssh";
+    togglable = false;
+    options = {
+      enable = lib.mkEnableOption "Enables ${name} related settings.";
+    };
+  };
+in (tools.fullModule {
+  inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
+  inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
+  inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  System = {
+    services.openssh.enable = true;
+    networking.firewall.allowedTCPPorts = [10022 22]; # 10022 to qemu
+  };
+})
--- sys/networking/vpn.nix
+++ sys/networking/vpn.nix
@@ -8,21 +8,12 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "networking";
-    subfolder = "networking";
+    name = "vpn";
     togglable = false;
     main-repo = "nix";
     branch = "latest";
-    imports = [
-      "vpn"
-      "ssh"
-    ];
     options = {
       enable = lib.mkEnableOption "Enables ${name} related settings.";
-    };
-    settings = {
-      vpn.enable = true;
-      ssh.enable = true;
     };
   };
 in (tools.fullModule rec {
@@ -30,26 +21,11 @@ in (tools.fullModule rec {
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
   System = {
-    networking = {
-      hostName = "NixOs"; # FEAT : make this part of baseSettings
-      networkmanager = {
-        enable = true;
-      };
-
-      firewall = rec {
-        enable = true;
-        allowedTCPPortRanges = [
-          {
-            from = 1714;
-            to = 1764;
-          }
-        ]; # FOR KDE CONNECT TODO: find a better way to do this
-        allowedUDPPortRanges = allowedTCPPortRanges; # FOR KDE CONNECT
-      };
-    };
     environment.systemPackages = with packages; [
-      iproute2
-      bridge-utils
+      openvpn
+    ];
+    networking.networkmanager.plugins = with packages; [
+      networkmanager-openvpn
     ];
   };
 })
