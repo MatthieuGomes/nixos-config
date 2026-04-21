3230139de98577809c536d40c10ed9d5fc8e81b7
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-20 22:38:02 -0400

    feat(git) : adding lazygit to git module

--- progs/dev.nix
+++ progs/dev.nix
@@ -15,7 +15,7 @@
     imports = [
       "boot"
       "network"
-      "git" #solo
+      "git"
       "virtualization"
       "lang"
     ];
@@ -31,7 +31,7 @@
       lang.enable = true;
     };
   };
-in (tools.fullModule rec {
+in (tools.fullModule {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
--- progs/dev/git.nix
+++ progs/dev/git.nix
@@ -18,14 +18,19 @@ in (tools.fullModule {
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
   Home = {
-    programs.git = {
-      enable = true;
-      settings = {
-        user = {
-          name = "MatthieuGomes";
-          email = "matthieu.gomes@ensea.fr";
+    programs = {
+      git = {
+        enable = true;
+        settings = {
+          user = {
+            name = "MatthieuGomes";
+            email = "matthieu.gomes@ensea.fr";
+          };
+          init.defaultBranch = "main";
         };
-        init.defaultBranch = "main";
+      };
+      lazygit = {
+        enable = true;
       };
     };
   };
