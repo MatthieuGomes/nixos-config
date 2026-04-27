8b89c93e4e917e0dcb0e836331bf8f87ad201834
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-04-27 13:54:15 -0400

    note(klassy) : adding a todo to configure and rebuild my own klassy pkgs

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nur-klassy-version = {
       url = "github:nix-community/nur/82a02f13454ca5b23248d9fe8fb15618a11b09f0";
       # sha256 = "0d8iwisgw15ivwbd9s041fbf33mqgccsmwxcvgwf3y84i2d1rb7a";
-    };
+    }; # TODO : Rebuild with my own + add options for configuration
     nur-unstable-pkgs = {
       url = "github:nix-community/nur";
       inputs.nixpkgs.follows = "unstablePkgs";
