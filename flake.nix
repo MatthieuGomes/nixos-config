34d073921fd5ec495a537047a7db508ad1b55a0a
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-05-12 16:43:57 -0400

    labels() : FIX WIP

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
         tuis.enable = true;
       };
       sys = {
-        label = "V.2.0_REFACORING_WIP";
+        label = "V.2.0_FIX_WIP";
         tags = [];
         version = nixos-version;
         main-user = "matthieu";
