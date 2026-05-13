14f5f9ecee6f9d1f32e73a1b9c131f12a8435dd2
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-05-13 15:42:29 -0400

    update(full) : update all dependencie and programs

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
         tuis.enable = true;
       };
       sys = {
-        label = "V.2.0_UPDATE_WIP";
+        label = "V.2.1";
         tags = [];
         version = nixos-version;
         main-user = "matthieu";
