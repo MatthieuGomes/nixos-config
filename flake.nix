316b166e189155f4b1dac33390d394fb246a3679
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-05-14 17:18:21 -0400

    note(completions) : detailed todos + checked completed tasks + upgrade version label

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
         tuis.enable = true;
       };
       sys = {
-        label = "V.2.2";
+        label = "V.2.2.1";
         tags = [];
         version = nixos-version;
         main-user = "matthieu";
