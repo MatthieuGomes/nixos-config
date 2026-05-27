2de1a34688813c7592980d1df05c154dcf252e26
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-05-27 15:41:51 -0400

    feat(partion-manager) : added kde partition manager

--- progs/dev/boot/kde-partition-manager.nix
+++ progs/dev/boot/kde-partition-manager.nix
@@ -8,26 +8,20 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "boot";
-    subfolder = "boot";
-    imports = [
-      "efibootmgr"
-      "gparted"
-      "os-prober"
-      "kde-partition-manager"
-    ];
+    name = "kde-partition-manager";
+    main-repo = "nix";
+    branch = "latest";
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
     };
-    settings = {
-      efibootmgr.enable = true;
-      gparted.enable = true;
-      os-prober.enable = true;
-      kde-partition-manager.enable = true;
-    };
   };
-in (tools.fullModule {
+in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  System = {
+    environment.systemPackages = with packages.kdePackages; [
+      partitionmanager
+    ];
+  };
 })
