f08ec12f966a3e5ba5915e0882816b37eb0f8249
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-06-05 17:38:58 -0400

    temp(fstab) : temporary deactivation of windows and shared partion mounting

--- sys/fstab.nix
+++ sys/fstab.nix
@@ -18,19 +18,19 @@ in (tools.fullModule {
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
   System = {
-    fileSystems = {
-      "Windows" = {
-        device = "/dev/disk/by-uuid/8EE66173E6615C89";
-        fsType = "ntfs";
-        mountPoint = "/mnt/windows";
-        options = ["defaults" "users" "nofail"];
-      };
-      "Shared" = {
-        device = "/dev/disk/by-uuid/3C32D71A32D6D7CA";
-        fsType = "ntfs";
-        mountPoint = "/mnt/shared";
-        options = ["defaults" "users" "nofail"];
-      };
-    };
+    # fileSystems = {
+    #   "Windows" = {
+    #     device = "/dev/disk/by-uuid/8EE66173E6615C89";
+    #     fsType = "ntfs";
+    #     mountPoint = "/mnt/windows";
+    #     options = ["defaults" "users" "nofail"];
+    #   };
+    #   "Shared" = {
+    #     device = "/dev/disk/by-uuid/3C32D71A32D6D7CA";
+    #     fsType = "ntfs";
+    #     mountPoint = "/mnt/shared";
+    #     options = ["defaults" "users" "nofail"];
+    #   };
+    # };
   };
 })
