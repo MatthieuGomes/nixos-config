4198f5caf503df3a9f2c1acaf5a888568d4b10f5
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-06-19 12:38:38 -0400

    feat(virtualization) : adding winboat WIP (needs 32 Go)

--- progs/dev/virtualization/winboat.nix
+++ progs/dev/virtualization/winboat.nix
@@ -1,3 +1,4 @@
+# ATTENTION : Besoin de 32 GIGA DE LIBRE !
 {
   config,
   lib,
@@ -8,26 +9,20 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "virtualization";
-    subfolder = "virtualization";
-    imports = [
-      "docker"
-      "qemu"
-      "virt-manager"
-      "winboat"
-    ];
+    name = "winboat";
+    main-repo = "nix";
+    branch = "unstable";
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
     };
-    settings = {
-      docker.enable = true;
-      qemu.enable = true;
-      virt-manager.enable = true;
-      winboat.enable = true;
-    };
   };
-in (tools.fullModule {
+in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  Home = {
+    home.packages = with packages; [
+      winboat
+    ];
+  };
 })
