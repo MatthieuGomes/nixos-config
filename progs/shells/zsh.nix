bd37e3dfd274bcbfae45bd802cc7c0a90c4330d6
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-07-02 12:06:10 -0400

    feat(nix-shell-zsh) : adding plugin to automatically login in zsh when doing nix-shell

--- progs/shells/zsh.nix
+++ progs/shells/zsh.nix
@@ -64,6 +64,16 @@ in (tools.fullModule rec {
           name = "fzf-tab";
           src = "${packages.zsh-fzf-tab}/share/fzf-tab";
         })
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = packages.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
       ];
       initContent =
         (tools.ifEnabled config "config.progs.shells.zsh.p10k"
