719a54ac1b7d47cfc383bc984465118368d91df3
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-07-02 12:05:18 -0400

    feat(obsidian) : adding obsidian

--- progs/editors/obsidian.nix
+++ progs/editors/obsidian.nix
@@ -8,24 +8,20 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "editors";
-    subfolder = "editors";
-    imports = [
-      "vim"
-      "vscode"
-      "obsidian"
-    ];
+    name = "obsidian";
+    main-repo = "nix";
+    branch = "latest";
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
     };
-    settings = {
-      vim.enable = true;
-      vscode.enable = true;
-      obsidian.enable = true;
-    };
   };
-in (tools.fullModule {
+in (tools.fullModule rec{
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  Home = {
+    home.packages = with packages; [
+      obsidian
+    ];
+  };
 })
