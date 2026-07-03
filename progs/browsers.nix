b3531313ad1f652eaed8c6b0ecaf69263b8e49e5
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-07-03 13:43:11 -0400

    todo(browsers) : default browser option to add

--- progs/browsers.nix
+++ progs/browsers.nix
@@ -16,6 +16,7 @@
       "zen"
     ];
     extras = {
+      # TODO : add a default browser setting to choose between firefox, chromium and zen
     };
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
@@ -26,8 +27,19 @@
       zen.enable = true;
     };
   };
-in (tools.fullModule {
+in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  # System = {
+  #   environment.sessionVariables.DEFAULT_BROWSER = "${pkgs-list.nix.latest.firefox}/bin/firefox";
+  #   xdg.mime.enable = true;
+  #   xdg.mime.defaultApplications = {
+  #     "text/html" = "firefox.desktop";
+  #     "x-scheme-handler/http" = "firefox.desktop";
+  #     "x-scheme-handler/https" = "firefox.desktop";
+  #     "x-scheme-handler/about" = "firefox.desktop";
+  #     "x-scheme-handler/unknown" = "firefox.desktop";
+  #   };
+  # };
 })
