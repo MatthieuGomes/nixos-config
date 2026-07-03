f7f8cb0f6169db2555a2a8121fed28e2f36df33a
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-07-03 15:58:16 -0400

    fix(migration) : fixed kwallet script

--- WIP/migration/kwallet_mig.sh
+++ WIP/migration/kwallet_mig.sh
@@ -9,16 +9,10 @@ KWALLETD_PATH="/home/$USER/.local/share/kwalletd"
 
 FILES="$KWALLETD_PATH/kdewallet.kwl $KWALLETD_PATH/kdewallet.salt $KWALLETD_PATH/kdewallet_attributes.json"
 
+sudo mkdir -p $NEW_SYSTEM_PATH$KWALLETD_PATH
+
 sudo rm -rf $NEW_SYSTEM_PATH$KWALLETD_PATH/*
 
-cp $FILES $NEW_SYSTEM_PATH$KWALLETD_PATH
+sudo cp -r $FILES $NEW_SYSTEM_PATH$KWALLETD_PATH
 
-sudo nixos-enter --root $NEW_SYSTEM_PATH
-
-chown -R $USER:$GROUP $KWALLETD_PATH
-
-chmod 755 $KWALLETD_PATH
-chmod 600 $FILES
-chmod 644 $KWALLETD_PATH/kdewallet.
-
-exit
+sudo nixos-enter --root $NEW_SYSTEM_PATH --command "chown -R $USER:$GROUP $KWALLETD_PATH && chmod 755 $KWALLETD_PATH && chmod 600 $FILES && chmod 644 $KWALLETD_PATH/kdewallet.*"
